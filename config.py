4552e480c977e941ed8e3ae40d0ebd2b1dd8425f
Moxyproxy1 <itzeehimansh.u@gmail.com>
2024-09-11 13:08:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7525339576:AAHeqCXHAJptVgo1sO7kZG0AJErrKFAu4Y0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Jatt")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6186321214")
+
+
+
+
+
+
+
+  
